75a4d44ea84f3e60ba254c6684c08a4ea89ff932
pjprasanna <pjprasanna19@gmail.com>
2023-07-30 17:23:23 +0530

    files uploaded

---
+++ rto_request.py
@@ -0,0 +1,66 @@
+import urllib3
+import psycopg2
+from bs4 import BeautifulSoup
+from requests import get
+urllib3.disable_warnings()
+
+#DB connection
+conn = psycopg2.connect(database ="postgres", user = "postgres",password = "passwd", host = "localhost",port = "5432")
+print("Connection Successful to PostgreSQL")
+cur = conn.cursor() 
+
+# Check if table exist
+cur.execute("select * from information_schema.tables where table_name=%s", ('rto_details_request',))
+if bool(cur.rowcount) == False:
+       cur.execute(f"""
+      CREATE TABLE rto_details_request
+      (
+      ID  SERIAL PRIMARY KEY,
+      state_name TEXT NOT NULL,
+      RTO_code TEXT NOT NULL,
+      rto_name TEXT NOT NULL,
+      Address TEXT NOT NULL,
+      Phone_number TEXT,
+      Office_timing TEXT
+      )""")
+       conn.commit()
+       print("Table Created successfully...")
+
+
+def rto_details(state_page,state):
+    for row in state_page.find(class_="state-offices").find_all(class_="state-offices-row"):
+        rto_name_and_code=row.find_all('p')
+        rto_name=rto_name_and_code[0].text
+        rto_link=row.find('a')['href']
+        rto_link="https://loconav.com"+rto_link
+        try:
+            response=get(rto_link,verify=False)
+            response.raise_for_status()
+            each_key=state[13:]
+            a=rto_name
+            detail = BeautifulSoup(response.text, 'html.parser')
+        except Exception as e:
+            print(f"Error occurred while retrieving RTO details: {e}")
+        rto_card_det=[]
+        for rto_det_row in detail.find(class_="card-details").find_all(class_="card-details-row"):
+            rto_card_det.append(rto_det_row.find_all('p')[1].text)
+        #print('RTO code :',rto_card_det[0],'\nRTO address :',rto_card_det[1],'\nRTO phone number :',rto_card_det[2],'\nRTO timing :',rto_card_det[3],'\n')
+        cur.execute(f"""INSERT INTO rto_details_request (state_name,RTO_code,rto_name,Address,Phone_number,Office_timing) VALUES ('{each_key}','{rto_card_det[0]}','{a}','{rto_card_det[1]}','{rto_card_det[2]}','{rto_card_det[3]}') """)
+        conn.commit()
+    print("\nRTO details stored for",each_key,"...")
+    
+def all_states():
+    response=get('https://loconav.com/rto-offices',verify=False)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    state_list=[]
+    state_link=[]
+    for link in soup.find(class_="clearfix statewise-rto").find_all('a'):
+        state_list.append(link.text)
+        state_link.append(link['href'])
+    for state in state_link:
+        #print("\nState name :",state[13:].upper(),'\n')
+        response2=get(f'https://loconav.com/{state}',verify=False)
+        state_page = BeautifulSoup(response2.text, 'html.parser')
+        rto_details(state_page,state)
+
+all_states()
---
+++ rto_selenium.py
@@ -0,0 +1,102 @@
+#Importing libraries
+import psycopg2
+from selenium import webdriver
+from configparser import ConfigParser
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+#Parser initializer
+configur = ConfigParser()
+configur.read('config.ini')
+
+#DB connection
+conn = psycopg2.connect(database ="postgres", user = "postgres",
+						password = "passwd", host = "localhost",
+						port = "5432")
+
+print("Connection Successful to PostgreSQL")
+cur = conn.cursor() 
+
+
+# Check if table exist
+cur.execute("select * from information_schema.tables where table_name=%s", ('rto_details_selenium',))
+if bool(cur.rowcount) == False:
+       cur.execute(f"""
+      CREATE TABLE rto_details_selenium
+      (
+      ID  SERIAL PRIMARY KEY,
+      state_name TEXT NOT NULL,
+      RTO_code TEXT NOT NULL,
+      rto_name TEXT NOT NULL,
+      Address TEXT NOT NULL,
+      Phone_number TEXT,
+      Office_timing TEXT
+      )""")
+       conn.commit()
+       print("Table Created successfully")
+
+#Chrome driver initialization
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options= chrome_options)
+driver.maximize_window()
+
+regional_name=[]
+result=[]
+
+driver.get('https://loconav.com/rto-offices/')
+
+configur.add_section('state')
+
+
+def statename_link():
+      for i in range(1,8):
+            for j in range(1,5):
+                  textbox=driver.find_elements(By.XPATH,'/html/body/div[2]/div/div/div[5]/div/div[' +str(i) + ']/div[' +str(j) + ']/a')
+                  for val in textbox:
+                        configur.set('state',val.text,val.get_attribute('href'))
+
+      with open("config.ini", 'w') as configfile:
+              configur.write(configfile)
+
+try:
+       statename_link()
+       print('State names are fetched...')
+except:
+       print('Failed to fetch state names...')
+       driver.close()
+
+def rto_details():
+      for each_section in configur.sections():
+             for (each_key, each_val) in configur.items(each_section):
+                    s=each_val
+                    driver.get(s)
+                    row=driver.find_elements(By.CLASS_NAME,'state-offices-row')
+                    for l in range(1,len(row)+1):
+                                                regional_office=driver.find_elements(By.XPATH,'/html/body/div[2]/div/div/div[5]/div/div[' +str(l) + ']/a')
+                                                for reg in regional_office:
+                                                      driver.implicitly_wait(10)
+                                                      regional_name.append(reg.text)
+                                                      reg.click()
+                                                      title=driver.find_elements(By.XPATH,'/html/body/div[2]/div/div/div[5]/div/h4')
+                                                      for ti in title:
+                                                            a=ti.text
+                                                            #print(a)
+                                                      for de in range(1,5):
+                                                            que=driver.find_elements(By.XPATH,'/html/body/div[2]/div/div/div[5]/div/div/div[' +str(de) + ']/p[' +str(1) + ']')
+                                                            ans=driver.find_elements(By.XPATH,'/html/body/div[2]/div/div/div[5]/div/div/div[' +str(de) + ']/p[' +str(2) + ']')
+                                                            for q in que:
+                                                                  for an in ans:
+                                                                        #print(q.text," :",an.text)
+                                                                        result.append(an.text) 
+                                                      cur.execute(f"""INSERT INTO rto_details_selenium (state_name,RTO_code,rto_name,Address,Phone_number,Office_timing) VALUES ('{each_key}','{result[0]}','{a}','{result[1]}','{result[2]}','{result[3]}') """)
+                                                      conn.commit()
+                                                      result.clear()
+                                                      driver.back()
+      driver.back()
+
+try:
+        rto_details()
+        print('RTO office details are stored...')
+except:
+        driver.close()
